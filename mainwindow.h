f2bca836928e6c80aad12643d30179002471ec62
Taiyaki <39448619+SnowyTaiyaki@users.noreply.github.com>
2026-04-17 13:14:58 -0400

    Update mainwindow.h

--- mainwindow.h
+++ mainwindow.h
@@ -5,6 +5,9 @@
 #include <QPushButton>
 #include <QLabel>
 #include <QVBoxLayout>
+#include <QComboBox>
+
+namespace Ui {class MainWindow;}
 
 // Main application window
 class MainWindow : public QMainWindow
@@ -20,14 +23,34 @@ public:
     ~MainWindow();
 
 private slots:
-    // Allows user to select an image and then displays the selected image
-    void openImage();
+    void openImage();               // Allows user to select an image and then displays the selected image
+    void openVideo();               // Allows the user to use video instead of image
+    void processImage();               // Allows the user to test the with machine learning
+    void openResults();             // Allows the user to open the results window
 
 private:
-    QWidget *centralWidget;         // Holds UI components
+    Ui::MainWindow *ui;
+
+    QWidget     *centralWidget;     // Holds UI components
+
     QVBoxLayout *mainLayout;        // Vertical layout
+
+    QComboBox *comboBox;            // Drop down box
+
     QPushButton *uploadButton;      // Allows the user to upload an image
-    QLabel *imageLabel;             // Label used to display the image
+    QPushButton *videoButton;       // Allow to user to turn on video
+    QPushButton *processButton;        // Allow the user to begin testing with machine learning
+    QPushButton *resultsButton;     // Allow the user to view the results after the image has been tested
+
+    QLabel      *imageLabel;        // Label used to display the image
+
+    QPixmap originalPixmap;         // Unscaled original image
+
+    QString currentImagePath;       // Current image path
+
+protected:
+    // Allow the image to be scaled
+     void resizeEvent(QResizeEvent *event) override;
 };
 
 #endif // MAINWINDOW_H
